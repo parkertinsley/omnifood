9209c3cad8b4033b05a6552c076989f63d10b352
Parkertinsley <parker.tinsley@gmail.com>
2020-06-20 15:55:54 -0600

    changed img hero to auto size

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -44,25 +44,25 @@ $(document).ready(function() {
     $('.js--wp-1').waypoint(function(direction) {
         $('.js--wp-1').addClass('animated fadeIn');
     }, {
-        offset: '50%'
+        offset: '90%'
     });
     
     $('.js--wp-2').waypoint(function(direction) {
         $('.js--wp-2').addClass('animated fadeInUp');
     }, {
-        offset: '50%'
+        offset: '90%'
     });
     
     $('.js--wp-3').waypoint(function(direction) {
         $('.js--wp-3').addClass('animated fadeIn');
     }, {
-        offset: '50%'
+        offset: '90%'
     });
     
     $('.js--wp-4').waypoint(function(direction) {
         $('.js--wp-4').addClass('animated pulse');
     }, {
-        offset: '50%'
+        offset: '90%'
     });
     
     
